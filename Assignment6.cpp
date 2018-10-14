954939a89ee52daa143dcdf267237f7785511364
AnthonyThe <39071032+AnthonyThe@users.noreply.github.com>
2018-10-14 06:37:15 -0700

    Update Assignment6.cpp

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include<ctime>
 #include<chrono>
-#include"windows.h"
+#include"windows.h"	//for the sleep function
 using namespace std;
 
 int findGCD(int num1, int num2);
@@ -11,11 +10,9 @@ int main()
 	int GCD;
 	int num1;
 	int num2;
-	double duration;
-	double start;
 	bool redo = true;
 	char answer;
-
+	
 	while (redo == true)
 	{
 
@@ -26,7 +23,7 @@ int main()
 		cout << "Number 2: ";
 		cin >> num2;
 
-		while (num1 < 1 || num2 < 1)
+		while (num1 < 1 || num2 < 1)	//doesn't test if the number goes too high
 		{
 			system("cls");
 			cout << "The numbers must be positive. Try again" << endl;
@@ -36,16 +33,16 @@ int main()
 			cin >> num2;
 		}
 
-		start = clock();
-
-		//Sleep(1000);	//this is to test the time, to make sure it's in milliseconds.
+		chrono::high_resolution_clock::time_point time1 = chrono::high_resolution_clock::now();
+		//Sleep(1000);	//this was to test the time
 
 		GCD = findGCD(num1, num2);
-
 		cout << "The greatest common denomenator is: " << GCD << endl << endl;
+		
+		chrono::high_resolution_clock::time_point time2 = chrono::high_resolution_clock::now();
+		chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(time2 - time1);
+		cout << "The program took " << time_span.count() << " seconds long" << endl << endl;
 
-		duration = clock() - start;
-		cout << "The program took " << duration << " milliseconds long" << endl << endl;
 		cout << "Would you like to try different numbers?('y' for yes, 'n' for no) ";
 		cin >> answer;
 		if (answer == 'n')
@@ -54,14 +51,13 @@ int main()
 		}
 		system("cls");
 	}
-	
+
 	return 0;
 }
 
 int findGCD(int num1, int num2)
 {
 	int remainder;
-	int quotient;
 
 	if (num1 > num2)
 	{
